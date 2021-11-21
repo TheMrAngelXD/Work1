1e809c7add02451a1f5ac5852decff6c62ab2a93
Stas <kostenko.stas@mail.ru>
2021-11-21 23:22:12 +0300

    Изменения кода

--- lab1.cpp
+++ lab1.cpp
@@ -2,32 +2,42 @@
 #include <ctime>
 using namespace std;
 
-int main()
-{
-    double* values;
-    int size;
-    cout << "n = ";
-    cin >> size;
-
-    if (size <= 0) {
-        cerr << "Invalid size" << endl;
-        return 1;
-    }
-    values = new double[size];
+int size;
+int rand(const int size, double* values) {
     srand(time(NULL));
     for (int i = 0; i < size; i++) {
         values[i] = rand();
-        cout << values[i]<<endl;
-        
+        cout << values[i] << endl;
     }
-    int max = values[0];
-    for (int i = 1; i < size; i++) {
+    return 0;
+}
+int maximum(const int size, const double* values) {
+    double max = values[1];
+    for (int i = 0; i < size; i++) {
         if (values[i] > max) {
             max = values[i];
         }
     }
     cout << "max = " << max << endl;
+    return 0;
+}
 
-    delete[] values;
+int main()
+{
+    cout << "n = ";
+    int size;
+    cin >> size;
+    if (size <= 0) {
+        throw std::out_of_range("Invalid size");
+    }
+    double* values;
+    values = new double[size];
+    rand(size, values);
+    maximum(size, values);
+    if (values != nullptr)
+    {
+        delete[] values;
+        values = nullptr;
+    }
     return 0;
 }
