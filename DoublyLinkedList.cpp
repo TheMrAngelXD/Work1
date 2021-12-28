58e54b184db0109c5ba9fcaf5e7699fe5cf8d4a2
Stas <kostenko.stas@mail.ru>
2021-12-28 17:36:56 +0300

    Корректировка кода API

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
-#include "DoublyLL.h"
+#include "DoublyLinkedList.h"
 
 using namespace std;
 
-List::List()
-{
-	head = tail = NULL;
-	count = 0;
-}
 List::~List()
 {
-	delAll();
+	while (this->getCount() != 0)
+	{
+		Element* temp = head->next;
+		if (head != nullptr)
+		{
+			delete head;
+			head = nullptr;
+		}
+		head = temp;
+		count--;
+	}
 }
--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -1,82 +1,98 @@
 ﻿#pragma once
 #include <iostream>
 using namespace std;
-/**
-	Задает элемент списка,
-	указатели на начальный и конечный элементы
- */
-struct Elem
+/*!
+	\brief Класс звена списка.
+
+	В данном классе реализована структура элемента списка.
+	В нем присутствуют приватные поля:
+	1. int data - значение элемента списка
+	2. Element *next - указатель типа Element на следующий элемент списка
+	3. Element *previous - указатель типа Element на предыдущий элемент списка.
+*/
+class Element
 {
 	int data;
-	Elem* head, * tail;
+	Element* next;
+	Element* previous;
+	friend class List;
+public:
+	Element(int data, Element* next, Element* previous)
+	{
+		this->next = next;
+		this->previous = previous;
+		this->data = data;
+	}
 };
-/**
-   Создание класса Список,
-   указатели на начало и конец списка
-   Счетчик на количество элементов в списке
+/*!
+	\brief Класс реализации списка.
+
+	В данном классе описана реализация двусвязного списка.
+	В нем присутствуют приватные поля:
+	1. Element *head, *tail - указатели типа Element на первый элемент списка,
+	на последний элемент списка.
+	2. size_t count - значение типа size_t количества элементов списка.
+	Методы, взаимодействующие с двусвязным списком.
 */
 class List
 {
-	Elem* head, * tail;
+	Element* head;
+	Element* tail;
 	size_t count;
+	/*!
+	 \brief Приватный метод получения элементов списка.
+
+	 Приватный метод получения элементов списка двусвязного списка.
+	 Его предназначение - дальнейшее использование в переопределении
+	 оператора сдвига <<.
+	*/
+	friend ostream& operator << (ostream& out, const List& list);
 
 public:
-	/**
-	Конструктор по умолчанию
- */
-	List();
-	/**
-	Конструктор копирования
- */
-	List(const List&);
-	/**
-	Деструктор
- */
+	/*!
+	 \brief Конструктор по умолчанию.
+
+	 В данном конструкторе присваиваются указателям
+	 head (начало списка) и tail (конец списка) nullptr (нулевые значения).
+	*/
+	List():head(nullptr), tail(nullptr) , count(0) {};
+	
+	/*!
+	 \brief Деструктор.
+
+	 В деструкторе присваиваются указателям элементам списка нулевые значения
+	 и освобождается память.
+	*/
 	~List();
 
-	/**
-	Получает количество элементов в списке
- */
-	size_t getCount();
-	/**
-	Получает элемент из списка по заданному индексу
- */
-	Elem* getElement(const size_t index);
+	/*!
+	 \brief Метод вывода количества элементов списка.
+	 \return count - Переменная типа size_t количества элементов списка
 
-	/**
-	Удаление всего списка
- */
-	void delAll();
-	/**
-	Удаление элемента,если нет параметра,
-	то происходит запрос
- */
-	void del(int pos = 0);
-	/**
-	Вставка элемента,если нет параметра,
-	то происходит запрос
- */
-	void insert(int pos = 0);
+	 Метод класса вывода количества элеметов двусвязного списка.
+	*/
+	const size_t getCount();
+	/*!
+	 \brief Метод класса добавления элемента в список.
+	 \param[in] const int data - значение,
+	 которое будет хранить добавленный элемент списке.
+	*/
+	void addElement(const int data);
+	/*!
+	 \brief Метод класса изменения значения элемента списка.
+	 \param[in] const size_t numberElement - номер элемента,который будет изменяться.
+	 \param[in] const int setData - значение элемента,который будет изменяться.
 
-	/**
-	Добавление элемента в конец списка
- */
-	void addTail();
+	 Метод класса изменения значения элемента циклического односвязного списка.
+	*/
+	void changeElement(const size_t numberElement, const int setData);
+	/*!
+	 \brief Метод класса удаления элемента списка.
+	 \param[in] const size_t numberElement - номер элемента, подверженному удалению,
+	 введеный с клавиатуры.
+	*/
+	void deleteElement(const size_t numberElement);
 
-	/**
-	Добавление элемента в начало списка
- */
-	void addHead();
-
-	/**
-	Вывод списка в консоль
- */
-	friend std::ostream& operator << (std::ostream& out, const List& list);
 };
 
-std::ostream& operator << (std::ostream& out, const List& list)
-{
-	out<< list;
-	return out;
-}
 	
